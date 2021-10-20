2eb220f293df4d72ebf73f10d94afba109d14737
happymonkey1 <brownejamessean@gmail.com>
2021-10-20 13:37:58 -0700

    inital fork (added premake and cleaned up files)

---
+++ premake5.lua
@@ -0,0 +1,37 @@
+project "RCCPP"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"**.h",
+		"**.cpp"
+	}
+
+	includedirs
+	{
+		"RCCPP/RuntimeCompiler",
+		"RCCPP/RuntimeobjectSystem"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "on"
+
+	filter "system:linux"
+		pic "on"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "on"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
